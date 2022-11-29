2e8263779cc22b0e87f2a1756de091f6ea1159a3
Christian Schudoma <cschu1981@gmail.com>
2022-11-29 14:50:09 +0100

    added lineage lookup

--- taxcheck/__main__.py
+++ taxcheck/__main__.py
@@ -5,7 +5,10 @@ import re
 import subprocess
 import sys
 
+from functools import lru_cache
+
 from Bio import Entrez
+from ete3 import NCBITaxa
 
 
 def get_lines_from_chunks(_in, bufsize=400000000):
@@ -29,12 +32,70 @@ def extract_read_ref_from_sam(stream):
         flag = int(aln[1])
         if flag & 0x1 and not (aln[2] == aln[6] or flag & 0x8):
             continue
-                                                                      
         tags = dict(item.split(":")[0::2] for item in aln[11:])
         if not tags.get("XA"):
-            read2ref.setdefault(aln[2], set()).add(aln[0])              
+            read2ref.setdefault(aln[2], set()).add(aln[0])
     return read2ref
 
+
+class LineageLookup:
+
+    TAXLEVELS = {
+        "kingdom": (0, "k"),
+        "phylum": (1, "p"),
+        "class": (2, "c"),
+        "order": (3, "o"),
+        "family": (4, "f"),
+        "genus": (5, "g"),
+        "species": (6, "s"),
+    }
+
+    def __init__(self):
+        self.ncbi = NCBITaxa()
+
+    @lru_cache(maxsize=1000)
+    def get_lineage(self, taxid):
+
+
+        def get_lineage_str(levels):
+            return ";".join(
+                f"{prefix}__{string}"
+                for prefix, string in sorted(levels)
+            )
+
+
+        try:
+            lineage = self.ncbi.get_lineage(taxid)
+        except ValueError:
+            return (taxid, "INVALID", "INVALID")
+
+        ranks = self.ncbi.get_rank(lineage)
+        rev_ranks = {v: k for k, v in ranks.items()}
+        names = self.ncbi.get_taxid_translator(lineage)
+
+        levels = []
+        for tid, tname in zip(lineage, self.ncbi_translate_to_names(lineage)):
+            rank = ranks.get(tid, "").replace("superkingdom", "kingdom")
+            try:
+                index, prefix = LineageLookup.TAXLEVELS.get(rank)
+            except TypeError:
+                continue
+            levels.append((index, prefix, str(tid), tname))
+
+        return get_lineage_str((index, prefix, tid) for index, prefix, tid, _ in levels), get_lineage_str((index, prefix, tname) for index, prefix, _, tname in levels)
+
+
+
+
+#>>> ncbi.get_lineage(662479)
+#[1, 131567, 2157, 28890, 2290931, 183963, 1644055, 1644056, 2251, 403181, 662479]
+#>>> ncbi.get_rank(ncbi.get_lineage(662479))
+#{1: 'no rank', 2157: 'superkingdom', 2251: 'genus', 28890: 'phylum', 131567: 'no rank', 183963: 'class', 403181: 'species', 662479: 'strain', 1644055: 'order', 1644056: 'family', 2290931: 'clade'}
+#>>> ncbi.translate_to_names(ncbi.get_lineage(662479))
+#['root', 'cellular organisms', 'Archaea', 'Euryarchaeota', 'Stenosarchaea group', 'Halobacteria', 'Haloferacales', 'Haloferacaceae', 'Haloferax', 'Haloferax mucosum', 'Haloferax mucosum ATCC BAA-1512']
+#>>> ncbi.get_taxid_translator(ncbi.get_lineage(662479))
+#{1: 'root', 2157: 'Archaea', 2251: 'Haloferax', 28890: 'Euryarchaeota', 131567: 'cellular organisms', 183963: 'Halobacteria', 403181: 'Haloferax mucosum', 662479: 'Haloferax mucosum ATCC BAA-1512', 1644055: 'Haloferacales', 1644056: 'Haloferacaceae', 2290931: 'Stenosarchaea group'}
+#
 
 
 
@@ -54,6 +115,7 @@ def main():
 
 
     ncbi_lookup = {}
+    lineage_lookup = LineageLookup()
 
     accessions = tuple(read2ref)
     chunksize = 20
@@ -79,7 +141,8 @@ def main():
 
         for acc in ids:
             mod_acc = re.sub(r"^ref\|", "", acc).strip("|")
-            print(acc, mod_acc, ncbi_lookup.get(mod_acc))
+            acc_data = ncbi_lookup.get(mod_acc)
+            print(acc, mod_acc, acc_data, *lineage_lookup.get_lineage(acc_data["taxid"]))
 
 #[
 #    {
@@ -98,10 +161,6 @@ def main():
 #        'Length': IntegerElement(423394, attributes={}), 'Status': 'live', 'ReplacedBy': '', 'Comment': '  ', 'AccessionVersion': 'NZ_FWXW01000002.1'
 #    }
 #]
-        
- 
-
-
 
 
 if __name__ == "__main__":
