5ad0d61294e0bd5b2bc079eebe71f1d7ab8d0a8a
Christian Schudoma <cschu1981@gmail.com>
2023-01-25 18:52:50 +0100

    refactor: ignore YP-tag taxids to avoid ete3 - reassignment - for real

--- taxcheck/__main__.py
+++ taxcheck/__main__.py
@@ -113,12 +113,7 @@ def main():
     for nreads, (rname, ref, mapq, yp_tag, xa_tag) in enumerate(extract_yp_reads_from_sam(sam_proc.stdout), start=1):
         if nreads % 1000000 == 0:
             print(f"Processed {nreads} reads.", file=sys.stderr)
-        # read2ref[rname] = {
-        #     "ref": ref,
-        #     "mapq": mapq,
-        #     "yp": yp_tag,
-        #     "xa": xa_tag,
-        # }
+        
         note = ""
         lcount = Counter()
         refs = (ref, ) + (tuple(item[0] for item in xa_tag) if xa_tag else tuple())
@@ -126,76 +121,25 @@ def main():
             acc =[x for x in gene_id.split("|") if x and x != "ref"]
             lcount[ncbi_lookup.setdefault(gene_id, get_tax_annotation(session, acc[0]))] += 1
 
-        # lcount[ncbi_lookup.get(ref, {}).get("taxid", -1)] += 1
-        # for gene_id in (ref, ) + (tuple(item[0] for item in xa_tag) if xa_tag else tuple()):
-
-        # refs.update((ref, ) + (tuple(item[0] for item in xa_tag) if xa_tag else tuple()))
-        # if args.debug and len(read2ref) > 10:
-        #     break
-
-        # # print(f"Looking up {len(refs)} taxonomy ids...", file=sys.stderr)
-        # # cached_ncbi = args.ncbi_cache
-        # # if args.ncbi_cache and os.path.isfile(cached_ncbi):
-        # #     with open(cached_ncbi) as _in:
-        # #         ncbi_lookup = json.load(_in)
-        # # else:
-        # #     ncbi_lookup = ncbi_tax_lookup(args.email, list(refs), chunksize=args.ncbi_chunksize)
-        # #     with open(f"{os.path.basename(args.bamfile)}.ncbi_cache.json", "wt") as _out:
-        # #         json.dump(ncbi_lookup, _out)
-        
-        # # for ref in refs:
-        # #     acc = [x for x in ref.split("|") if x and x != "ref"]
-        # #     taxid = get_tax_annotation(session, acc[0])
-        # #     ncbi_lookup.setdefault(ref, {})["taxid"] = taxid
-        # #     with open(f"{os.path.basename(args.bamfile)}.ncbi_cache.json", "wt") as _out:
-        # #         json.dump(ncbi_lookup, _out)
-
-        # print("Annotating reads...", file=sys.stderr)
-        # for rname, aln_data in read2ref.items():
-        #     note = ""
-
-        #     rrefs = ((aln_data["ref"],) + (tuple(r for r, _ in aln_data["xa"]) if aln_data["xa"] else tuple()))
-
-        #     if args.debug:
-        #         print(rname, aln_data, file=sys.stderr)
-        #         print("RREFS", rrefs, file=sys.stderr)
-        #         print("CHECK", aln_data["ref"] in ncbi_lookup, file=sys.stderr)
-
-        #     # count how often a read aligns against a refseq from a specific taxonomy
-        #     lcount = Counter()
-        #     for ref in rrefs:
-        #         if args.debug:
-        #             print(ref, ncbi_lookup.get(ref), file=sys.stderr)
-        #         lcount[ncbi_lookup.get(ref, {}).get("taxid", -1)] += 1
-
-        #     if args.debug:
-        #         print("LCOUNT", lcount, file=sys.stderr)
-        #         print("LCOUNT HAS -1", -1 in lcount, file=sys.stderr)
-
-        # generate lineages from each reference and store the alignment counts along with it
-        lineages2 = {
+        lineages = {
             taxid: {
                 "lineage": lfactory.generate_lineage(taxid),
-                "count": lcount[taxid],
+                "count": count,
             }
-            for taxid in yp_tag  # aln_data["yp"]
+            for taxid, count in lcount.items()
         }
 
-        if args.debug:
-            print(*lineages2.values(), sep="\n", file=sys.stderr)
-            # print(*(f"{l.get_string()}: {c}" for l, c in lineages2.values()), sep="\n", file=sys.stderr)
-
-        # determine a consensus line based on fraction of alignments
-        if len(lineages2) == 1:
-            consensus_lineage = tuple(lineages2.values())[0]["lineage"]
+        if len(lineages) == 1:
+            consensus_lineage = tuple(lineages.values())[0]["lineage"]
             consensus_level = "species"
             consensus_id, consensus_name = consensus_lineage.levels[-1].values()
         else:
+
             # iterate from species -> kingdom
             for level in range(Lineage.TAXLEVELS["species"][0], -1, -1):
                 tax_counter = Counter()
                 # for each level count the taxids (multiplied by number of alignments)
-                for lineage_data in lineages2.values():
+                for lineage_data in lineages.values():
                     lineage, count = lineage_data["lineage"], lineage_data["count"]
                     level_id = lineage.levels[level]["id"] if lineage.levels[level] is not None else -1
                     tax_counter.update((level_id,) * count)
@@ -214,9 +158,65 @@ def main():
                         consensus_id, consensus_name = consensus_lineage.levels[level].values()
                         break
 
+
+        # lcount[ncbi_lookup.get(ref, {}).get("taxid", -1)] += 1
+        
+        #     # count how often a read aligns against a refseq from a specific taxonomy
+        #     lcount = Counter()
+        #     for ref in rrefs:
+        #         if args.debug:
+        #             print(ref, ncbi_lookup.get(ref), file=sys.stderr)
+        #         lcount[ncbi_lookup.get(ref, {}).get("taxid", -1)] += 1
+
+        #     if args.debug:
+        #         print("LCOUNT", lcount, file=sys.stderr)
+        #         print("LCOUNT HAS -1", -1 in lcount, file=sys.stderr)
+
+        # generate lineages from each reference and store the alignment counts along with it
+        # lineages2 = {
+        #     taxid: {
+        #         "lineage": lfactory.generate_lineage(taxid),
+        #         "count": lcount[taxid],
+        #     }
+        #     for taxid in yp_tag  # aln_data["yp"]
+        # }
+
+        # if args.debug:
+        #     print(*lineages2.values(), sep="\n", file=sys.stderr)
+        #     # print(*(f"{l.get_string()}: {c}" for l, c in lineages2.values()), sep="\n", file=sys.stderr)
+
+        # determine a consensus line based on fraction of alignments
+        # if len(lineages2) == 1:
+        #     consensus_lineage = tuple(lineages2.values())[0]["lineage"]
+        #     consensus_level = "species"
+        #     consensus_id, consensus_name = consensus_lineage.levels[-1].values()
+        # else:
+        #     # iterate from species -> kingdom
+        #     for level in range(Lineage.TAXLEVELS["species"][0], -1, -1):
+        #         tax_counter = Counter()
+        #         # for each level count the taxids (multiplied by number of alignments)
+        #         for lineage_data in lineages2.values():
+        #             lineage, count = lineage_data["lineage"], lineage_data["count"]
+        #             level_id = lineage.levels[level]["id"] if lineage.levels[level] is not None else -1
+        #             tax_counter.update((level_id,) * count)
+        #         if args.debug:
+        #             print("LEVEL", level, tax_counter, file=sys.stderr)
+        #         # then check if there's a consensus (based on fractional representation by the alignments)
+        #         if tax_counter:
+        #             top_taxid, top_count = tax_counter.most_common()[0]
+        #             cutoff = args.species_cutoff if level == Lineage.TAXLEVELS["species"][0] else args.lineage_cutoff
+        #             if top_taxid == -1:
+        #                 if not note:
+        #                     note = f"FIRST_LEVEL_UNKNOWN={list(Lineage.TAXLEVELS)[level]}"
+        #             elif top_count / sum(tax_counter.values()) > cutoff:
+        #                 consensus_lineage = lfactory.generate_lineage(top_taxid)
+        #                 consensus_level = list(Lineage.TAXLEVELS)[level]
+        #                 consensus_id, consensus_name = consensus_lineage.levels[level].values()
+        #                 break
+
         print(
             rname,
-            len(lineages2),
+            len(lineages),
             # len(aln_data["xa"]) + 1,
             len(xa_tag) + 1,
             consensus_level,
